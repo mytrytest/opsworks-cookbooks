01292aa2813ddbfc7fdc41d2bebbf1f0ed625dd7
Rohit Vyas <mytrytest@gmail.com>
2021-01-19 11:12:10 +0530

    update prisma to prisma1

--- deploy/recipes/prismaInstall.rb
+++ deploy/recipes/prismaInstall.rb
@@ -1,7 +1,7 @@
 include_recipe 'deploy'
 Chef::Log.info("prisma install")
 
-execute("sudo npm install -g prisma")
+execute("sudo npm install -g prisma1")
 execute(" sudo npm install -g dotenv")
 #execute("cd #{deploy[:current_path]} && sudo docker-compose up -d")
 
